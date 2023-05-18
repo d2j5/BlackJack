1db17ea7ca0667cc864a58e9df2de4ffe7d09587
Javier <d2j5@hotmail.com>
2023-05-17 22:18:11 -0400

    Initial commit

---
+++ src/Card.java
@@ -0,0 +1,33 @@
+public class Card {
+    //vars
+    private Suit suit;
+    private Rank rank;
+
+    //create a card given a suit and a value.
+
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    //Generate getters
+    public int getValue() {
+        return rank.rankValue;
+    }
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+    public String toString() {
+     return ("[" + rank +" of " + suit + "] (" +this.getValue()+")" );
+    }
+
+    public Card(Card card){
+        this.suit = card.getSuit();
+        this.rank = card.getRank();
+    }
+}
